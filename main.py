419e13a5210bc8f257dbeea29c0b71fc6aeda473
Kush Shah <kshah2@wpi.edu>
2022-02-21 15:08:19 -0500

    implemented qTable class that extends dict

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main(filename,time_to_run,probablity_moving,constant_reward):
+from qtable import QTable
 
-    print("Hello World!")
+
+def main(filename=None, time_to_run=None, probablity_moving=None, constant_reward=None):
+    qtable = QTable()
+    qtable['0,0'] = [0, 0, 0, 0]
+    print(qtable.get('0,0'))
 
 
 if __name__ == "__main__":
