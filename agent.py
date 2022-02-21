1341841b548fccee7a691afa3870f39d5190e38b
keithdesantis <kwdesantis@wpi.edu>
2022-02-21 18:19:30 -0500

    commented

--- agent.py
+++ agent.py
@@ -9,9 +9,21 @@ from time import time
 
 
 class Agent:
+    """
+    Class that represents the agent that is learning
+    """
     def __init__(self, qtable, input_board, time_to_run, prob_moving, constant_reward, step_size=0.1, gamma=1):
+        """
+        :param qtable: The lookup table that the agent will use to track estimated state-action values
+        :param input_board: The baord object which the agent is learning on, used to check when the path terminates
+        :param time_to_run: The time the agent should run for
+        :param prob_moving: The chance that the agent will move in the desired direction when it moves
+        :param constant_reward: The movement penalty for each action the agent takes
+        :param step_size: The alpha parameter that indicates what proportion of the observed difference in state-action utilities the agent records
+        :param gamma: The gamma parameter, used to discredit future information
+        """
         self.qtable = qtable
-        self.dirs = [0,1,2,3]
+        self.dirs = [0,1,2,3] # This list is used to represent the four actions, [Up, Right, Down, Left]
         self.input_board = input_board
         self.board_row_max = self.input_board.rows - 1
         self.board_col_max = self.input_board.cols -1
@@ -20,8 +32,8 @@ class Agent:
         self.constant_reward = constant_reward
         self.step_size = step_size
         self.gamma = gamma
-        self.epsilon_greedy = 0.1
-        self.row, self.col = None, None
+        self.epsilon_greedy = 0.1 # The chance that the agent will take a random action rather than the estimated ideal action
+        self.row, self.col = None, None # The agent's current position
 
     def move(self, action_int):
         """
@@ -40,9 +52,9 @@ class Agent:
         if random.random() < self.prob_moving:
             return action_int
         elif random.random() < 0.5:
-            return self.dirs[(action_int + 1) % len(self.dirs)]
+            return self.dirs[(action_int + 1) % len(self.dirs)] # This deflects the agent to the right
         else:
-            return self.dirs[action_int - 1]
+            return self.dirs[action_int - 1] # This deflects the agent to the left
 
     def get_coords_after_action(self, row, column, action):
         """
@@ -78,12 +90,13 @@ class Agent:
             balls = 1
         previous_utility = self.qtable[self.row, self.col][action]
         next_state = self.get_coords_after_action(self.row, self.col, action)
+        # Below is the equation used for Q search from class
         self.qtable[self.row, self.col][action] = previous_utility + self.step_size*(self.constant_reward +
                                            self.gamma * max(self.qtable[next_state[0], next_state[1]]) - previous_utility)
 
     def generate_start_state(self):
         """
-        Generates a random start state
+        Generates a random start state (not a terminal state)
         """
         row = random.randint(0, self.board_row_max)
         column = random.randint(0, self.board_col_max)
@@ -93,22 +106,25 @@ class Agent:
         return row, column
 
     def run(self):
+        """
+        The agent runs simulations and learns, updating its qTable as it goes
+        """
         end = time() + self.time_to_run
 
         while time() < end:
-            self.row, self.col = self.generate_start_state()
+            self.row, self.col = self.generate_start_state() # While we still have time, run another simulation
 
-            while not self.input_board.check_if_terminal(self.row, self.col):
+            while not self.input_board.check_if_terminal(self.row, self.col): # Search until we find a terminal
 
-                if random.random() < self.epsilon_greedy:
+                if random.random() < self.epsilon_greedy: # If the agent is going to explore rather than exploit
                     action_to_take = self.qtable[self.row, self.col].index(random.choice(self.qtable[self.row, self.col]))
-                else:
+                else: # Take the ideal action
                     action_to_take = self.qtable.max_action(self.row, self.col)
 
-                self.update_utility(action_to_take)
+                self.update_utility(action_to_take) # Update last utility
 
-                self.move(action_to_take)
+                self.move(action_to_take) # Make the move
 
 
-        self.input_board.set_to_directional(self.qtable)
+        self.input_board.set_to_directional(self.qtable) # This function prepares the board object for final output printing
         return self.input_board
--- boardgenerator.py
+++ boardgenerator.py
@@ -3,6 +3,11 @@ import random
 
 
 def generate_board(dimension, filenumber):
+    """
+    Generates a random board
+    :param dimension: The dimension of the board (X by X)
+    :param filenumber: The number that will be used to give the board a unique filename "boardX.txt"
+    """
 
     file = open("board" + str(filenumber) + ".txt", 'w')
 
@@ -13,9 +18,10 @@ def generate_board(dimension, filenumber):
             board[row].append(0)
 
     number_of_terminals = random.randint(1,math.ceil((dimension*dimension) * 0.05))
+    # Ensures there are terminals, anywhere from 1 to 5% of all states will be terminals
 
     for terminal in range(number_of_terminals):
-        terminal_num = random.randint(-10,10)
+        terminal_num = random.randint(-10,10) # Sets the terminals to a value between -10 and 10
         board[random.randrange(dimension)][random.randrange(dimension)] = terminal_num
 
     for row in range(dimension):
